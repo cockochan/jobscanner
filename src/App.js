1352787a3174c5d71312f541f0f2f23b81a42e7a
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2021-04-04 15:01:46 +0000

    color schemes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const extraction_result = keyword_extractor.extract(jobText,{
                                                        
   return (
     <Container fluid>
-        <h6 >by Constantin Dimitrenco</h6>
+        <h6 className='dark'>by Constantin Dimitrenco</h6>
         <Navbar  bg="primary" variant="dark"><Col>
   <Navbar.Brand href="#home"><h1 className="hugeTitle">Job desciption scanner® </h1>
    
@@ -29,11 +29,13 @@ const extraction_result = keyword_extractor.extract(jobText,{
 <h4 className="text-center dark">can be used for any keywords now only cv's</h4>
      <p className="text-center"> Wish you find a better job useing JOBSCANNER®</p>
       <Form><Col>
+      
       <Form.Row className='ml-0 mt-5'><textarea type='textarea' style={{minHeight:'350px',width:'100%'}}  onChange={e=>setJobText(e.target.value)} placeholder='paste job description here'></textarea>  </Form.Row>
       </Col>
       </Form><div style={{justifyItems:'space-around'}}>
+      <h4 className="dark mt-2 ml-3">Extracted keywords:</h4>
 {
-  extraction_result.map(el=><Button variant="outline-success" className='mt-3 ml-3'>{el}</Button>)
+  extraction_result.map(el=><Button variant="outline-primary" className='mt-3 ml-3'>{el}</Button>)
 }</div></Container>
  </Container>
   );
