3286658bff568482d963fac59e649f422bc2541c
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2021-04-04 13:04:23 +0000

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
+import keyword_extractor from 'keyword-extractor'
+import {FormControl,Navbar,Form,Col, Container} from 'react-bootstrap'
 function App() {
+  const [jobText, setJobText]=useState('')
+
+
+
+const extraction_result = keyword_extractor.extract(jobText,{
+                                                                language:"english",
+                                                                remove_digits: true,
+                                                                return_changed_case:true,
+                                                                remove_duplicates: true
+ 
+                                                           });
+                                                       
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+     <Navbar bg="light" expand="lg">
+ I am extractor i do extract
+      </Navbar>
+      <Form><Col>
+      <Form.Row><FormControl type='textarea' onChange={e=>setJobText(e.target.value)}></FormControl>  </Form.Row>
+      <div></div>
+      </Col>
+      </Form><ul>
+{
+  extraction_result.map(el=><li key={el}>{el}</li>)
+}
+</ul> </Container>
   );
 }
 
