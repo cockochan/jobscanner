4d75440d8e9b44cdec41e34449085d5f67431b20
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2021-04-11 21:03:14 +0000

    fff

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Constantin Dimitrenco/i);
   expect(linkElement).toBeInTheDocument();
 });
